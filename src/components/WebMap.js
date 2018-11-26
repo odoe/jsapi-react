3a5da1319830adb11525d1a4f86e089e6c07153c
rene rubalcava <rrubalcava@esri.com>
2018-11-26 08:09:37 -0800

    add clean up for useEffect

--- src/components/WebMap.js
+++ src/components/WebMap.js
@@ -4,12 +4,14 @@ export function WebMap() {
   const elementRef = useRef();
 
   useEffect(_ => {
+    let cleanup;
     // lazy load the module that loads the JSAPI
     // and initialize it
     import("../data/app").then(
-      app => app.initialize(elementRef.current)
+      app => cleanup = app.initialize(elementRef.current)
     );
-  });
+    return () => cleanup && cleanup();
+  }, []);
 
   // assign elementRef to the ref of our component
   return (
